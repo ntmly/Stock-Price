915d75fa0daf7a554214941009fe84ccbf6cc487
ntmly <160614057+ntmly@users.noreply.github.com>
2025-10-23 15:48:17 +0700

    initial

--- get_vietnam_stock_data.py
+++ get_vietnam_stock_data.py
@@ -7,7 +7,7 @@ tickers = ["VIC", "VHM", "VNM", "HPG", "VCB", "BID", "CTG", "MBB", "TCB", "FPT"]
 
 for code in tickers:
     stock = Vnstock().stock(symbol=code, source='VCI')
-    df = stock.quote.history(start='2020-01-01', end='2025-10-01')
+    df = stock.quote.history(start='1990-01-01', end='2025-10-01')
     df = df[['time', 'open', 'high', 'low', 'close', 'volume']]
     df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
     df.to_csv(f"datack/{code}.csv", index=False)
